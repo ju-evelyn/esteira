396387377368db0b0979931e39d6f7800ac326eb
Júlia Evelyn <72927574+ju-evelyn@users.noreply.github.com>
2022-04-29 14:31:06 -0300

    adição da classe recepcao

--- Recepcao.java
+++ Recepcao.java
@@ -1,3 +1,24 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 public class Recepcao {
-    
+    private Esteira esteira;
+    public Recepcao(){
+        esteira = new Esteira();
+    }
+    try{
+        File dados = new File("dados_tp01.txt");
+        Scanner leitor = new Scanner(dados);
+        int totalDePedidos = Integer.parseInt(leitor.nextLine());
+        while(leitor.hasNextLine()){
+            String[] novoPedido = leitor.nextLine().split(";");
+            int numProdutos = Integer.parseInt(novoPedido[1]);
+            int prazo = Integer.parseInt(novoPedido[2]);
+            this.esteira.addPedido(novoPedido[0], numProdutos, prazo);
+            
+        }
+        leitor.close();
+    }catch(FileNotFoundException e1){
+        System.out.println("Arquivo não encontrado");
+    }
 }
